4eb19e8f97745a206203354de3e44c4360b2f0c3
Fluzze <joseantoniopregofragacerceda@gmail.com>
2024-11-21 20:54:44 +0100

    enemigo implementado,spikes y plantilla de has muerto creada

--- platformer/enemy.gd
+++ platformer/enemy.gd
@@ -1,25 +1,46 @@
 extends CharacterBody2D
+class_name Enemy
 
+@export_category("Config")
+@export_group("Options")
+@export var health : int = 1
+@export var score : int = 100
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export_group("Motion")
+@export var speed : int = 16
+@export var gravity : int = 16
 
+var direction : int = 1
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _process(delta: float) -> void:
+	if health > 0:
+		motion_ctrl()
+			
+func motion_ctrl() -> void:
+	$AnimatedSprite2D.scale.x = direction
+	
+	if not $AnimatedSprite2D/RayCast2D.is_colliding() or is_on_wall():
+		direction *= -1
+	velocity.x = direction * speed
+	velocity.y += gravity
 	move_and_slide()
+
+func damage_ctrl(damage : int) -> void:
+	health -= damage
+	
+	if health <= 0:
+		$AnimatedSprite2D.play("death")
+		$CollisionShape2D.set_deferred("disabled",true)
+		gravity = 0
+	
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "death":
+		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player and health > 0:
+		body.damage_ctrl()
+		
+		
